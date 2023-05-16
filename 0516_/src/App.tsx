87b15df314f6c6169af76ec8403cdaf14c7ebedf
[SW4/1반]신성민 <ssongpig@naver.com>
2023-05-16 10:30:57 +0900

    feat: 복습 및 카운터 & 스탭

--- 0516_/src/App.tsx
+++ 0516_/src/App.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+function CounterState() {
+	const [count, setCount] = useState(0);
+	const [step, setStep] = useState(1);
+	return (
+		<div>
+			<h1>Counter - state, Form</h1>
+			<input
+				type='number'
+				value={step}
+				onChange={(e) => {
+					setStep(Number(e.target.value));
+				}}
+			/>
+			<button
+				onClick={() => {
+					setCount((count===undefined ? 0:count)+step);
+				}}
+			>
+				+
+			</button>{' '}
+			{count}
+		</div>
+	);
+}
 function App() {
-	return <div></div>;
+	return <CounterState />;
 }
 
 export default App;
