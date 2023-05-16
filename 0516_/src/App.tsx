edaaf38017f773ba9947022064ca4717b7e3f617
[SW4/1반]신성민 <ssongpig@naver.com>
2023-05-16 12:16:12 +0900

    feat: mui 설치, setInterval start,stop키

--- 0516_/src/App.tsx
+++ 0516_/src/App.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import { Counter4UseEffect } from './Counter4UseEffect';
 import { Counter1 } from './Counter1';
+import { Container, Grid } from '@mui/material';
 
 function App() {
 	return (
-		<div>
-			<Counter4UseEffect />
-			<Counter1 />
-		</div>
+		<Container>
+			<Grid
+				container
+				spacing={2}
+			>
+				<Grid
+					item
+					xs={12}
+					sm={6}
+				>
+					<Counter4UseEffect />
+				</Grid>
+				<Grid
+					item
+					xs={12}
+					sm={6}
+				>
+					<Counter1 />
+				</Grid>
+			</Grid>
+		</Container>
 	);
 }
 
--- 0516_/src/Counter4UseEffect.tsx
+++ 0516_/src/Counter4UseEffect.tsx
@@ -1,26 +1,43 @@
 import React, { useEffect, useState } from 'react';
 
-export function Counter4UseEffect() {
-	const [count, setCount] = useState(0);
+//useEffect의 첫번째 파라미터 -> 콜백함수
+//콜백함수의 return값-> 함수
+//함수는 컴포넌트가 제거될때 호출
+//컴포넌트가 지워지면 useEffect가 재실행
+
+export const Counter4UseEffect = () => {
+	const [count, setCount] = useState<number>(0);
+	const [isRunning, setIsRunning] = useState<Boolean>(false);
+	// isRunning: true
 	useEffect(() => {
-		const id = setInterval(() => {
-			// setCount(count + 1);
-			setCount((oldCount) => oldCount + 1);
-		}, 1000);
-		console.log('interval', id);
+		let id: string | number | NodeJS.Timer | undefined;
+		if (isRunning) {
+			id = setInterval(() => {
+				// setCount(count + 1);
+				setCount((oldCount) => oldCount + 1);
+			}, 1000);
+		}
 		return () => {
-			console.log('unmount', id);
-			clearInterval(id);
-			//useEffect의 첫번째 파라미터 -> 콜백함수
-			//콜백함수의 return값-> 함수
-			//함수는 컴포넌트가 제거될때 호출
-			//컴포넌트가 지워지면 useEffect가 재실행
+			if (id) {
+				clearInterval(id);
+			}
 		};
-	}, []);
+	}, [isRunning]);
+
+	const startCount = () => {
+		setIsRunning(true);
+	};
+
+	const stopCount = () => {
+		setIsRunning(false);
+	};
+
 	return (
-		<div>
+		<div className='layout'>
 			<h1>useEffect, setInterval</h1>
 			{count}
+			<button onClick={startCount}>Start</button>
+			<button onClick={stopCount}>Stop</button>
 		</div>
 	);
-}
+};
