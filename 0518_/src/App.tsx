8c132f5ffb6e60195a038fdbceffd76b9b5e53e6
[SW4/1반]신성민 <ssongpig@naver.com>
2023-05-18 10:18:47 +0900

    feat : 파일 수정

--- 0518_/src/App.tsx
+++ 0518_/src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
+function Counter1() {
+	const count = 10;
+	return (
+		<div>
+			<h1>Counter</h1>
+			<button>+</button>
+			{count}
+		</div>
+	);
+}
+function Counter2() {
+	const count = 10;
+	return <div>{count}</div>;
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<Counter1></Counter1>
+			<Counter2></Counter2>
+		</div>
+	);
 }
 
 export default App;
